fbaed1decf4003c926bfd80f7acb7c1ec1fa30a8
Alex Walbom <alexwalbom@gmail.com>
2023-03-04 13:27:51 -0700

    added flowers; added fetch function and display

--- build.js
+++ build.js
@@ -1,8 +1,38 @@
+const plants = [
+    {url: './plants/roseflower.png', name: 'rose'},
+    {url: './plants/tulipflower.png', name: 'tulip'},
+    {url: './plants/sunflowerflower.png', name: 'sunflower'}
+];
 
+function fetchPlants(){
+    fetch('./plants/')
+    .then(response => response.text())
+    .then(data => {
+        const parser = new DOMParser();
+        const html = parser.parseFromString(data, 'text/html');
+        const fileLinks = html.querySelectorAll('a[href$="flower.jpg"]');
+        fileLinks.forEach(link => {
+        const url = link.getAttribute(href);
+        const name = url.split('/').pop().split('.')[0];
+        plants.push({url: url, name: name});
+        });
+    });
+}
 
+function displayCurrentPlant(){
+    currentImgEl.src = plants[plantIndex];
+}
+
+fetchPlants();
+displayCurrentPlant();
 const plantButton = document.querySelector('#plantbutton');
 const leftButton = document.querySelector('#ptleft');
 const rightButton = document.querySelector('#ptright');
+const currentImgEl = document.querySelector('#currentImg');
+let plantIndex = 0;
 
 
-plantButton.addEventListener('click', savePlant())
+
+
+leftButton.addEventListener('click', )
+//plantButton.addEventListener('click', savePlant())
