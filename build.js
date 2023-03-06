354c7fd59b6ff08f04f9fd82f31323efcf1bcd8c
Alex Walbom <alexwalbom@gmail.com>
2023-03-06 11:54:49 -0700

    finished javascript

--- build.js
+++ build.js
@@ -12,27 +12,33 @@ const currentImgEl = document.querySelector('#currentImg');
 let plantIndex = 0;
 
 function fetchPlants(){
-    fetch('./plants/')
-    .then(response => response.text())
-    .then(data => {
-        const parser = new DOMParser();
-        const html = parser.parseFromString(data, 'text/html');
-        const fileLinks = html.querySelectorAll('a[href$="flower.jpg"]');
-        fileLinks.forEach(link => {
-        const url = link.getAttribute(href);
-        const name = url.split('/').pop().split('.')[0];
-        plants.push({url: url, name: name});
-        });
-    });
+    const plants = [
+        {url: './plants/roseflower.png', name: 'rose'},
+        {url: './plants/tulipflower.png', name: 'tulip'},
+        {url: './plants/sunflowerflower.png', name: 'sunflower'}
+    ];
+    return plants;
+    // fetch()
+    // .then(response => response.text())
+    // .then(data => {
+    //     const parser = new DOMParser();
+    //     const html = parser.parseFromString(data, 'text/html');
+    //     const fileLinks = html.querySelectorAll('a[href$="flower.jpg"]');
+    //     fileLinks.forEach(link => {
+    //     const url = link.getAttribute(href);
+    //     const name = url.split('/').pop().split('.')[0];
+    //     plants.push({url: url, name: name});
+    //     });
+    // });
 }
 
 function displayCurrentPlant(){
-    currentImgEl.src = plants[plantIndex].url;
+    currentImgEl.src = fetchPlants()[plantIndex].url;
 }
 
 function leftImage(){
     if(plantIndex === 0){
-        plantIndex = plants.length - 1;
+        plantIndex = fetchPlants().length - 1;
     }
     else {
         plantIndex--;
@@ -41,7 +47,7 @@ function leftImage(){
 }
 
 function rightImage(){
-    if(plantIndex === (plants.length - 1)){
+    if(plantIndex === (fetchPlants().length - 1)){
         plantIndex = 0;
     }
     else{
@@ -51,7 +57,7 @@ function rightImage(){
 }
 
 function savePlant(){
-    localStorage.setItem('plant_' + plants[plantIndex].name, JSON.stringify(plants[plantIndex]));
+    localStorage.setItem('plant_' + fetchPlants()[plantIndex].name, JSON.stringify(plants[plantIndex]));
     window.location.href = "garden.html";
 }
 
