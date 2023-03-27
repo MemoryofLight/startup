7850d833e6b77c9fa51caaedbce8d02031be0084
Alex Walbom <alexwalbom@gmail.com>
2023-03-26 20:08:11 -0600

    added a bunch of login.js stuff

--- public/login.js
+++ public/login.js
@@ -1,7 +1,77 @@
-function login(){
-    const element = document.querySelector("#user");
-    localStorage.setItem('username', element.value);
-    const ele = document.querySelector('#pass');
-    localStorage.setItem('password', ele.value);
-    window.location.href = "garden.html";
+//called immediately on creation
+(async () => {
+    let authenticated = false;
+    const username = localStorage.getItem('username');
+    if(username){
+        const nameEl = document.querySelector('#username');
+        nameEl.value = username;
+        const auth = await getUser(nameEl.value);
+        authenticated = auth?.authenticated;
+    }
+
+    if(authenticated) {
+        document.querySelector('#playerName').textContent = username;
+        setDisplay('loginControls', 'none');
+        setDisplay('enterControls', 'block');
+    } else {
+        setDisplay('loginControls', 'block');
+        setDisplay('enterControls', 'none');
+    }
+})(); //what does this parenthesis mean?
+
+async function loginUser() {
+    loginOrCreate(`/api/auth/login`);
+}
+
+async function createUser() {
+    loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint){
+    const username = document.querySelector('#username')?.value;
+    const password = document.querySelector('#userPassword')?.value;
+    const response = await fetch(endpoint, {
+        method: 'POST',
+        body: JSON.stringify({email: username, password: password}),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        }
+    });
+    const body = await response.json();
+
+    if(response?.status === 200){
+        localStorage.setItem('username', username);
+        window.location.href = 'garden.html';
+    } else {
+        const modalEl = document.querySelector('#msgModal');
+        modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+        const msgModal = new bootstrap.Modal(modalEl, {});
+        msgModal.show()
+    }
+}
+
+function enter(){
+    window.location.href = 'garden.html';
+}
+
+function logOut(){
+    fetch(`/api/auth/logout`, {
+        method: 'delete',
+    }).then(() => (window.location.href = '/'));
+}
+
+async function getUser(email){
+    const response = await fetch(`/api/user/${email}`);
+    if(response.status === 200) {
+        return response.json();
+    }
+
+    return null;
+}
+
+function setDisplay(controlId, display){
+    const enterEl = document.querySelector(`#${controlId}`);
+    if(enterEl) {
+        enterEl.style.display = display;
+    }
 }
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+
+const authCookieName = 'token';
 
 // The service port. In production the application is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
@@ -15,22 +19,90 @@ app.use(express.static('public'));
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+//Create an Auth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if(await DB.getAuth(req.body.email)){
+    res.status(409).send({msg: 'Hey now, that username is taken...'});
+  } else {
+    const auth = DB.createAuth(req.body.email, req.body.password);
+
+    setAuthCookie(res, auth.token);
+
+    res.send({
+      id: user._id
+    });
+  };
+});
+
+//Get Auth token for login credentials
+apiRouter.post('/auth/login', async (req, res) => {
+  const auth = await DB.getAuth(req.body.email);
+  if(auth) {
+    if(await bcrypt.compare(req.body.password, user.password)){
+      setAuthCookie(res, user.token);
+      res.send({id: user_id});
+      return;
+    }
+  }
+  res.status(401).send({msg: 'Girl you are not allowed in. See yourself out before I do'});
+})
+
+//Delete auth token on logout
+apiRouter.delete('/auth/logout', (req, res) => {
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+})
+
+//return useless info about an 'auth'
+apiRouter.get('/user/:email', async (req,res) => {
+  const auth = await DB.getAuth(req.params.email);
+  if (auth){
+    const token = req?.cookies.token;
+    res.send({email: auth.email, authenticated: token === auth.token});
+    return;
+  }
+  res.status(404).send({msg: 'It seems your requested info does not exist'})
+})
+
 // GetPlants
 apiRouter.get('/plants', (_req, res) => {
   res.send(plants);
 });
 
-apiRouter.get('/garden/:username', async (req, res) => {
+//Secure router that verifies your credentials
+var secureApiRouter = express.Router(); //why var here?
+apiRouter.use(secureApiRouter); //any request that goes through api will first go through secure
+
+//use means that every request will go here regardless of what it is
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const auth = await DB.getAuthByToken(authToken);
+  if(auth){
+    next();
+  } else {
+    res.status(401).send({msg: 'You are not welcome'});
+  }
+})
+
+//IDK WHICH FUNCTIONS I NEED TO CONVERT INTO SECURE
+
+//Get the user's garden from storage
+secureApiRouter.get('/garden/:username', async (req, res) => {
   const user = await DB.getUser(req.params.username);
   res.send(user.garden);
 })
 
 // Posts the user's garden to storage.
-apiRouter.put('/gardens', async (req, res) => {
+secureApiRouter.put('/gardens', async (req, res) => {
   console.log('made it to gardens api');
   const state = await DB.updateGarden(req.body.username, req.body.plantname, req.body.del, req.body.id);
   res.send(200); //is this right?
 });
+
+//Error handler (errors will be passed down until they reach this middleware)
+app.use(function (err, req, res, next) {
+  res.status(500).send({type: err.name, message: err.message});
+})
 
 // Return the application's default page if the path is unknown
 app.use((_req, res) => {
@@ -41,6 +113,14 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict'
+  })
+}
 
 let plants = [
   {url: './plants/rose.png', name: 'rose'},
