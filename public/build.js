b9d3cf72ae1df94d1876383272abd20461ad280f
Alex Walbom <alexwalbom@gmail.com>
2023-04-01 20:52:26 -0600

    added lots of websocket stuff

--- public/build.js
+++ public/build.js
@@ -4,6 +4,10 @@ const leftButton = document.querySelector('#ptleft');
 const rightButton = document.querySelector('#ptright');
 const currentImgEl = document.querySelector('#currentImg');
 
+let socket;
+
+const PlantEvent = 'Planted';
+
 let plantIndex = 0;
 let plants;
 
@@ -40,7 +44,7 @@ async function rightImage(){
     await displayCurrentPlant();
 }
 
-function getPlayerName(){
+function getPlayerName(){ //should I update this to get it from the database?
     console.log(localStorage.getItem('username'));
     return localStorage.getItem('username');
 }
@@ -63,12 +67,48 @@ async function savePlant(){
             id: null
             })
         });
+        broadcastEvent(socket, getPlayerName(), PlantEvent, plants[plantIndex].name);
         window.location.href = "garden.html";
     } catch {
         savePlantLocal();
     }
 }
 
+  // Functionality for peer communication using WebSocket
+
+function configureWebSocket(){
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+    socket.onopen = (event) => {
+      displayMsg('system', 'plants', 'being tended');
+    };
+    socket.onclose = (event) => {
+      displayMsg('system', 'plants', 'absorbing sunshine');
+    };
+    socket.onmessage = async (event) => {
+      const msg = JSON.parse(await event.data.text());
+      if (msg.type === PlantEvent) {
+        displayMsg('Gardener', msg.from, `planted a ${msg.plant}`);
+    };
+  }
+}
+
+function displayMsg(cls, from, msg) { //do I need to pass socket here?
+    const chatText = document.querySelector('#player-messages');
+    chatText.innerHTML =
+      `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
+  }
+
+function broadcastEvent(socket, from, type, plant) {
+    const event = {
+      from: from,
+      type: type,
+      plant: plant,
+    };
+    socket.send(JSON.stringify(event));
+  }
+
+configureWebSocket();
 fetchPlants();
 leftButton.addEventListener('click', leftImage);
 rightButton.addEventListener('click', rightImage);
